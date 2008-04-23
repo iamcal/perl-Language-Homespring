eb45648a7bd86d5e1505461705146410c8b2c63e
Cal Henderson <cal@iamcal.com>
2008-04-23 04:40:36 +0000

    tagged 0.03

---
+++ examples/run.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Language::Homespring;
+use Data::Dumper;
+
+my $filename = $ARGV[0];
+die "please specify a file to read!\n" unless $filename;
+
+open(F, $filename) or die "couldn't read file $filename: $!";
+my $code = join '', <F>;
+close(F);
+
+my $hs = new Language::Homespring();
+$hs->parse($code);
+$hs->run(100);
+
+#print Dumper($hs->{root_node}, $hs->{salmon}, $hs->{new_salmon});
